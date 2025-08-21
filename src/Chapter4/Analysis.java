e32d726abbe93949faa3e72dc802971c960f3c68
Michael <mikechibike14@gmail.com>
2025-08-21 13:57:27 +0100

    init commit

--- src/Chapter4/Analysis.java
+++ src/Chapter4/Analysis.java
@@ -11,6 +11,12 @@ public class Analysis {
         while (studentCounter <= 10) {
             System.out.print("Enter result(1 = pass, 2 =fail): ");
             int result = input.nextInt();
+            while (result != 1 && result !=2){
+                System.out.println("Invalid number");
+                System.out.print("Enter result(1 = pass, 2 =fail): ");
+                result = input.nextInt();
+
+            }
 
             if (result == 1){
                 passes += 1;
