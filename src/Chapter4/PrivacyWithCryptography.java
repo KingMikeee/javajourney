a44c72939b490be4498281533320da573a1fe93c
Michael <mikechibike14@gmail.com>
2025-08-27 14:51:04 +0100

    init commit

--- src/Chapter4/PrivacyWithCryptography.java
+++ src/Chapter4/PrivacyWithCryptography.java
@@ -2,16 +2,11 @@ package Chapter4;
 
 import java.util.Scanner;
 
-public class Factorial {
+public class PrivacyWithCryptography {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter number: ");
+        System.out.println("Enter 4 digit number: ");
         int num = input.nextInt();
-
-        while (num > -1){
-         // i dont understand it
-
-        }
 
     }
 }
