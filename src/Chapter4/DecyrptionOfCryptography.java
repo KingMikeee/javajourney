64ffbdd7c3698d678291d1e2681e0631021a8e07
Michael <mikechibike14@gmail.com>
2025-08-27 16:18:51 +0100

    init commit

---
+++ src/Chapter4/DecyrptionOfCryptography.java
@@ -0,0 +1,31 @@
+package Chapter4;
+
+import java.util.Scanner;
+
+public class DecyrptionOfCryptography {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 4 digit number to decrypt: ");
+        int num = input.nextInt();
+        while (num < 1000 || num > 9999){
+            System.out.println("Error!! Enter 4 digit number: ");
+            num = input.nextInt();
+        }
+
+        int first = num / 1000;
+        int second = (num / 100) % 10;
+        int third = (num / 10) % 10;
+        int fourth = num % 10 ;
+
+        first = (first + 10 - 7) % 10;
+        second = (second + 10 - 7) % 10;
+        third = (third + 10 - 7) % 10;
+        fourth = (fourth + 10 - 7) % 10;
+
+        System.out.printf("The Decrypted number is %d%d%d%d%n", third, fourth, first, second);
+
+
+        System.out.println("Done");
+    }
+}
+
--- src/Chapter4/PrivacyWithCryptography.java
+++ src/Chapter4/PrivacyWithCryptography.java
@@ -7,6 +7,23 @@ public class PrivacyWithCryptography {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 4 digit number: ");
         int num = input.nextInt();
+        while (num < 1000 || num > 9999){
+            System.out.println("Error!! Enter 4 digit number: ");
+            num = input.nextInt();
+        }
 
+            int first = num / 1000;
+            int second = (num / 100) % 10;
+            int third = (num / 10) % 10;
+            int fourth = num % 10 ;
+
+            first = (first + 7) % 10;
+            second = (second + 7) % 10;
+            third = (third + 7) % 10;
+            fourth = (fourth + 7) % 10;
+
+
+            System.out.printf("The Encrypted number is %d%d%d%d%n", third, fourth, first, second);
+        System.out.println("Done");
     }
 }
