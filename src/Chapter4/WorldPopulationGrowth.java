d87583d8b57441e8e0793eb70bd1e28127f96955
Michael <mikechibike14@gmail.com>
2025-08-27 19:30:38 +0100

    init commit

--- src/Chapter4/WorldPopulationGrowth.java
+++ src/Chapter4/WorldPopulationGrowth.java
@@ -15,7 +15,7 @@ public class WorldPopulationGrowth {
         System.out.println("Year               Population                Population increase");
 
         while (year <= 75){
-            double population1 = (long)(p * (1 + r));
+            double population1 = (p * (1 + r));
             long newpop = (long) population1;
             long increase = newpop - p;
 
@@ -23,7 +23,19 @@ public class WorldPopulationGrowth {
 
             p = newpop;
             year++;
-
         }
+        long pop = 8235252251L;
+        int yearr = 0;
+        double rate = 0.0085;
+        long doublepop = (2 * pop);
+
+        while (pop < doublepop) {
+            long popu = (long)(pop * rate);
+            pop += popu;
+            yearr++;
+        }
+        System.out.println(" ");
+        System.out.println("The Population doubles after " + yearr + " years.");
+
 }
 }
