54aeee807b4837880f536136647a2528e818c6ef
Michael <mikechibike14@gmail.com>
2025-09-03 15:06:20 +0100

    init commit

---
+++ src/Chapter5/Sum.java
@@ -0,0 +1,12 @@
+package Chapter5;
+
+public class Sum {
+    public static void main(String[] args) {
+        int total = 0;
+
+        for (int number = 2; number <= 20; number +=2 ){
+            total += number;
+        }
+        System.out.printf("The Sum is %d%n", total);
+    }
+}
