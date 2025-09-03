c581c35f310984fbdb7f85546a7eda4853e8bc95
Michael <mikechibike14@gmail.com>
2025-09-03 21:38:26 +0100

    init commit

--- src/Chapter5/AutoPolicyTest.java
+++ src/Chapter5/AutoPolicyTest.java
@@ -1,4 +1,15 @@
 package Chapter5;
 
 public class AutoPolicyTest {
+    public static void main(String[] args){
+AutoPolicy policy1 = new AutoPolicy(11111111, "Lamborghini Urus", "NJ");
+AutoPolicy policy2 = new AutoPolicy(22222222, "Ford fushion", "ME");
+
+policyInNoFaultState(policy1);
+policyInNoFaultState(policy2);
+    }
+    public static void policyInNoFaultState(AutoPolicy policy) {
+        System.out.println("The Auto policy:");
+        System.out.printf("Account #: %d; Car %s;%nState %s %s a no fault state%n%n",policy.getAccountNumber(), policy.getMakeAndModel(), policy.getState(), (policy.isNoFaultState() ? "is": "is not"));
+    }
 }
