a0cd696fa180add542e70a983f84d7338c68b9ad
Michael <mikechibike14@gmail.com>
2025-09-23 17:05:11 +0100

    init commit

--- src/Chapter5/TrianglePattern.java
+++ src/Chapter5/TrianglePattern.java
@@ -6,13 +6,12 @@ public class TrianglePattern {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= i; j++)
                 System.out.print('*');
+            System.out.print(" ");
+
+            for (int j = i; j <= 10; j++)
+                System.out.print('*');
             System.out.println();
         }
-        System.out.println();
-for (int i = 10; i >= 1; i--) {
-        for (int j = 1; j <= i; j++)
-            System.out.print('*');
-        System.out.println();
+
     }
-}
 }
